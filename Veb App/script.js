d3cc0d2b7e16d3f8bb144fa86a1818b811e9725b
umaola <65196319+umaola@users.noreply.github.com>
2022-12-14 15:35:10 +0100

    Add files via upload

---
+++ Veb App/script.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const currentYear = new Date().setFullYear(2022);
+const newYearTime = new Date('January 1, 2023 00:00:00');
+
+
+setInterval(function updateCountdownTime() {
+    const currentTime = new Date();
+    const diff = (newYearTime - currentTime);
+
+    const d = Math.floor(diff/1000/60/60/24);
+    const h = Math.floor(diff/1000/60/60) % 24;
+    const m = Math.floor(diff/1000/60) % 60;
+    const s = Math.floor(diff/1000) % 60;
+    
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML=m < 10 ? '0' + m : m;
+    seconds.innerHTML= s < 10 ? '0' + s : s;
+
+}, 1000);
+
+updateCountdownTime();
+
